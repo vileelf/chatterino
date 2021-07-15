0e38e907fcc5dfca7ba0748dadcb1064a3e37ce3
vileelf <vileele@hotmail.com>
2021-07-14 21:14:43 -0400

    setting emote scale changed to false after reloading emotes

--- Chatterino.Common/TwitchChannel.cs
+++ Chatterino.Common/TwitchChannel.cs
@@ -1321,6 +1321,7 @@ namespace Chatterino.Common
                     Task.Run(() =>
                     {
                         Emotes.LoadGlobalEmotes();
+                        AppSettings.EmoteScaleChanged = false;
                     });
                 }
                 
