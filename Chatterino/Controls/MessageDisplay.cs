c9533b9e5deea198ab5140af7bc43acbd035713b
vileelf <vileele@hotmail.com>
2021-07-17 18:37:32 -0400

    Created a dynamic version of the emote list popup to allow chatterino to display arbritrary information.

---
+++ Chatterino/Controls/MessageDisplay.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chatterino.Common;
+
+namespace Chatterino.Controls
+{
+    public class MessageDisplay : MessageContainerControl
+    {
+        private List<Message> _messages = new List<Message>();
+        
+        public HashSet<LazyLoadedImage> GifEmotes = new HashSet<LazyLoadedImage>();
+        
+
+        protected override Message[] Messages
+        {
+            get
+            {
+                return _messages.ToArray();
+            }
+        }
+
+        public MessageDisplay()
+        {
+            mouseScrollMultiplyer = 0.2;
+
+            AllowMessageSeperator = false;
+            EnableHatEmotes = false;
+        }
+
+        public delegate void MessageAdder(List<Message> msgs, HashSet<LazyLoadedImage> gifemotes);
+        
+        public void LoadMessages(MessageAdder msgfunc)
+        {
+            lock (MessageLock)
+            {
+                var messages = _messages;
+                LazyLoadedImage twitchemote;
+                messages.Clear();
+                GifEmotes.Clear();
+                
+                msgfunc(messages, GifEmotes);
+
+                scrollAtBottom = false;
+                _scroll.Value = 0;
+                updateMessageBounds();
+                Invalidate();
+            }
+        }
+    }
+}
---
+++ Chatterino/Controls/MessageDisplayPopup.cs
@@ -0,0 +1,67 @@
+﻿using Chatterino.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatterino.Controls
+{
+    public class MessageDisplayPopup : Form
+    {
+        MessageDisplay container;
+
+        public MessageDisplayPopup()
+        {
+            Icon = App.Icon;
+
+            Text = "Message display";
+
+            //TopMost = true;
+
+            container = new MessageDisplay();
+            container.Dock = DockStyle.Fill;
+
+            Controls.Add(container);
+
+            Width = 500;
+            Height = 600;
+
+            TopMost = AppSettings.WindowTopMost;
+
+            AppSettings.WindowTopMostChanged += (s, e) =>
+            {
+                TopMost = AppSettings.WindowTopMost;
+            };
+        }
+        
+        public HashSet<LazyLoadedImage> GetGifEmotes() {
+            if (container!=null) {
+                return container.GifEmotes;
+            }
+            return null;
+        }
+        
+        public void ClearGifEmotes() {
+            if (container!=null) {
+                container.GifEmotes.Clear();
+            }
+        }
+        
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            container.GifEmotes.Clear();
+            container.ClearBuffer();
+            container = null;
+            base.OnFormClosed(e);
+        }
+
+        public void LoadMessages(string title, MessageDisplay.MessageAdder msgfunc)
+        {
+            Text = title;
+
+            container.LoadMessages(msgfunc);
+        }
+    }
+}
