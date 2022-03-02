abad8b83bab04a359f1de7616491bd9b62bb885f
vileelf <vileele@hotmail.com>
2022-03-02 11:48:13 -0500

    now auto rejecting accounts with a different client id from our client

--- Chatterino.Common/AccountManager.cs
+++ Chatterino.Common/AccountManager.cs
@@ -58,7 +58,7 @@ namespace Chatterino.Common
 
         public static void AddAccount(Account account)
         {
-            if (!string.IsNullOrWhiteSpace(account.Username))
+            if (!string.IsNullOrWhiteSpace(account.Username) && account.ClientId.Equals(IrcManager.DefaultClientID))
             {
                 _accounts[account.Username.ToLowerInvariant()] = account;
             }
