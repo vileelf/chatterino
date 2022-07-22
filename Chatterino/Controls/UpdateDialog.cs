15e71e703651c9284d557b309f7879530a9c23f8
vileelf <vileele@hotmail.com>
2022-07-22 00:40:22 -0400

    Made the patch notes support darkmode

--- Chatterino/Controls/UpdateDialog.cs
+++ Chatterino/Controls/UpdateDialog.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using Chatterino.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,15 @@ namespace Chatterino.Controls
             txtPatchNotes.ReadOnly = true;
             txtPatchNotes.Text = patchNotes;
             txtPatchNotes.Font = new Font(txtPatchNotes.Font.FontFamily, 10);
-            txtPatchNotes.BackColor = Color.White;
+            if (AppSettings.IsLightTheme()) {
+                txtPatchNotes.BackColor = Color.White;
+            } else {
+                txtPatchNotes.BackColor = Color.Black;
+                txtPatchNotes.ForeColor = Color.White;
+                this.BackColor = Color.FromArgb(unchecked((int)0xFF595959));
+                lblVersion.ForeColor = Color.White;
+            }
+            
             lblVersion.Text = "Chatterino V" + versionNumber + " is available Patchnotes:";
             Icon = App.Icon;
         }
